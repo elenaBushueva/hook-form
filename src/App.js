763f5accba7064a03d47f3d56eb76652a636fb7d
elenaBushueva <helenabushueva@gmail.com>
2022-04-26 12:10:38 -0700

    useForm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useForm} from "react-hook-form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, submitCount},
+        watch,
+    } = useForm({mode: "onChange"});
+
+    const onSubmit = data => console.log('Sent:', data);
+    const name = watch('name');
+
+    console.log('Name:' + name);
+
+    console.log(errors);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>FORM</h1>
+            <input
+                defaultValue="John"
+                {...register('name', {required: true, maxLength: 5})}
+            />
+            {errors.name && <i>name is required, 5 symbols max</i>}
+            <br/>
+            <input
+                {...register('age', {required: true, pattern: /\d?\d/, min: 18, max: 99})}
+            />
+            {errors.age && <i>age is required 18 - 99</i>}
+            <br/>
+            <select {...register('answer')}>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+            </select>
+            <br/>
+            <input type="submit"/>
+            <br/>
+            How many times the form being sent? {submitCount}
+        </form>
+    );
 }
 
 export default App;
